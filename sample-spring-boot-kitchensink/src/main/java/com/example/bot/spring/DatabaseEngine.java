05399a2fc11b81993caaadd0aafe73305e9e0bd9
tavish <tavish.gobindram@gmail.com>
2017-09-17 23:37:30 +0800

    fixed testcase and added a new one

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DatabaseEngine.java
@@ -34,9 +34,9 @@ public class DatabaseEngine {
 			br = new BufferedReader(isr);
 			String sCurrentLine;
 			
-			while (result != null && (sCurrentLine = br.readLine()) != null) {
+			while (result == null && (sCurrentLine = br.readLine()) != null) {
 				String[] parts = sCurrentLine.split(":");
-				if (text.toLowerCase().equals(parts[0].toLowerCase())) {
+				if (text.toLowerCase().contains(parts[0].toLowerCase())) {
 					result = parts[1];
 				}
 			}
--- sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
+++ sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
@@ -74,4 +74,17 @@ public class KitchenSinkTester {
 		assertThat(!thrown).isEqualTo(true);
 		assertThat(result).isEqualTo("def");
 	}
+	
+	@Test
+	public void testSubstr() throws Exception {
+		boolean thrown = false;
+		String result = null;
+		try {
+			result = this.databaseEngine.search("abcd");
+		} catch (Exception e) {
+			thrown = true;
+		}
+		assertThat(!thrown);
+		assertThat(result.equals("def"));
+	}
 }
